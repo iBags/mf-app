82b760513ad8b8e55c7ba231aa58423f5c8ab8b7
Gaby Carbajal <gabyalvarez159@gmail.com>
2025-08-25 17:53:29 -0500

    feat: added header component

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import {BrowserRouter} from "react-router-dom";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>This is my awesome Container</h1>
-      <p>Update a message for trigger github actions :D</p>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div>
+        <h1 style={{ textAlign: 'center' }}>This is my awesome Container</h1>
+        <p>Update a message for trigger github actions :D</p>
+        <hr />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
