6cbabeac89653bee5e5b410a80610c0be8e96de7
Gaby Carbajal <gabyalvarez159@gmail.com>
2025-08-25 19:19:57 -0500

    fix: style collision between apps fixed

--- container/src/App.js
+++ container/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import {BrowserRouter} from "react-router-dom";
+
 import Header from "./components/Header";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <div>
-        <h1 style={{ textAlign: 'center' }}>This is my awesome Container</h1>
-        <p>Update a message for trigger github actions :D</p>
-        <hr />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <div>
+          <h1 style={{ textAlign: 'center' }}>This is my awesome Container</h1>
+          <p>Update a message for trigger github actions :D</p>
+          <hr />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
